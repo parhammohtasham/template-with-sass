ed4f70cf8bb7f5532a5d22a5d5e804dc993b7b74
parhammohtasham <206984336+parhammohtasham@users.noreply.github.com>
2025-07-29 09:13:10 -0700

    navbar finally change

--- js/app.js
+++ js/app.js
@@ -5,3 +5,32 @@ const navToggle = document.querySelector("#nav-toggle");
 navToggle.addEventListener("click" , function(){
     navMenu.classList.toggle("show-menu");
 });
+
+// header style
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if (this.scrollY>=80) nav.classList.add('scroll-header'); 
+    else nav.classList.remove('scroll-header')
+}
+window.addEventListener("scroll", scrollHeader);
+
+// scroll sections active link
+// scroll section active link
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
